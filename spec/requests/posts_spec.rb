ad26a4a30c403b92112fe77230182feb31b4062e
Abenezer-Tilahun <abenezermesafint2128@gmail.com>
2022-06-10 00:51:13 +0300

    Add a basic test for post index, posts#show controller

---
+++ spec/requests/posts_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+RSpec.describe 'Posts', type: :request do
+  describe 'GET /index' do
+    before(:example) { get '/users/1/posts' }
+    it 'respond with message success' do
+      expect(response).to have_http_status(:ok)
+    end
+    it 'renders index view' do
+      expect(response).to render_template(:index)
+    end
+    it 'renders correct content' do
+      expect(response.body).to include('Posts of a given user')
+    end
+  end
+
+  describe 'GET /show' do
+    before(:example) { get '/users/1/posts/2' }
+    it 'respond with message success' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'renders show view' do
+      expect(response).to render_template(:show)
+    end
+
+    it 'renders correct content' do
+      expect(response.body).to include('Specific post of a user')
+    end
+  end
+end
