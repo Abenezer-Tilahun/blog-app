42ad25f4d46e98ec5a7264b3971f689668a37780
Abenezer-Tilahun <abenezermesafint2128@gmail.com>
2022-06-10 00:15:53 +0300

    Add post route, controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get 'users', to: 'users#index'
   get 'users/:user_id', to: 'users#show'
+  get 'users/:user_id/posts', to: 'posts#index'
+  get 'users/:user_id/posts/:post_id', to: 'posts#show'
 end
