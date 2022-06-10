8a9345cfe10b9502e9168433e659058d1aec6a19
Abenezer-Tilahun <abenezermesafint2128@gmail.com>
2022-06-10 21:02:59 +0300

    Address the requested change

--- db/migrate/20220610054628_create_post.rb
+++ db/migrate/20220610054628_create_post.rb
@@ -1,7 +1,6 @@
 class CreatePost < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
-      t.references :user, null: false, foreign_key: true
       t.references :author, foreign_key: { to_table: 'users' }
       t.string :title
       t.text :text
