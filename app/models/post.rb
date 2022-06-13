2c2beb9e7311ad9f81411a751466cf7411a06bd7
Abenezer-Tilahun <abenezermesafint2128@gmail.com>
2022-06-13 18:12:36 +0300

    Add Rails callback after_save on counter

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,11 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :comments
   has_many :likes
+
+  after_save :update_posts_counter
+
+  private
+
   def update_posts_counter
     author.increment!(:posts_counter)
   end
