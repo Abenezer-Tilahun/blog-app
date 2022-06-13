0b9d03a114677bffb887a468a646f381fd2b4b9f
Abenezer-Tilahun <abenezermesafint2128@gmail.com>
2022-06-13 17:10:19 +0300

    Modfiy the model and migration data to apply CRUD methods

--- db/migrate/20220610054921_create_comment.rb
+++ db/migrate/20220610054921_create_comment.rb
@@ -2,7 +2,7 @@ class CreateComment < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
       t.references :post, null: false, foreign_key: true
-      t.references :author, foreign_key: { to_table: 'users' }
+      t.references :author, foreign_key: { to_table: :users  }
       t.string :text
 
       t.timestamps
--- db/migrate/20220610170108_add_user_ref.rb
+++
@@ -1,5 +0,0 @@
-class AddUserRef < ActiveRecord::Migration[7.0]
-  def change
-    add_reference :posts :comments :likes, :author, foreign_key: { to_table: :users }
-  end
-end
