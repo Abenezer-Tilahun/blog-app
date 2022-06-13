69867050b122eb5d148651ec366e0af46d367480
Abenezer-Tilahun <abenezermesafint2128@gmail.com>
2022-06-13 17:34:26 +0300

    Add custom methods for models

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,9 @@
 class Like < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
+  def update_likes_counter
+    post.update(
+      likes_counter: post.likes.count
+    )
+  end
 end
