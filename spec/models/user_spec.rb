d00f03f28233bb0e1b8c69c1d53c4948981010a7
Abenezer-Tilahun <abenezermesafint2128@gmail.com>
2022-06-17 13:54:31 +0300

    Add unit test for all custom method in user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe 'validations for User model' do
+  describe 'validations for User model and all posts' do
     before(:each) do
-      @user = User.new(name: 'Abeni')
+      @user = User.new(name: 'Abeni', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.',
+                       posts_counter: 0)
+      5.times do |i|
+        Post.new(title: "Post #{i}", text: "Post #{i} text", comments_counter: 0, likes_counter: 0,
+                 author_id: @user.id)
+      end
+      @posts = Post.where(author_id: @user.id).all
     end
 
     before { @user.save }
@@ -21,6 +27,11 @@ RSpec.describe User, type: :model do
     it 'PostsCounter must be greater than or equal to zero' do
       @user.posts_counter = 7
       expect(@user).to be_valid
+    end
+
+    it 'should return less than 3 posts ' do
+      value = @user.recent_posts.length
+      expect(value).to be < 3
     end
   end
 end
