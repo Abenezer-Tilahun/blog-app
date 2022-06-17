a6a76b8aec80da11f675bf079e7242e033995305
Abenezer-Tilahun <abenezermesafint2128@gmail.com>
2022-06-17 12:07:52 +0300

    Make sure that the N+1 problem is solved

---
+++ spec/helpers/user_helper_spec.rb
@@ -0,0 +1,6 @@
+# Specs in this file have access to a helper object that includes
+# the UserHelper. 
+require 'rails_helper'
+RSpec.describe UserHelper, type: :helper do
+  pending "add some examples to (or delete) #{__FILE__}"
+end 
