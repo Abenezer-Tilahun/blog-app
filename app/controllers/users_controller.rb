216a30b64d35b1bbd165417a9bb21eb02ccab5dc
Abenezer-Tilahun <abenezermesafint2128@gmail.com>
2022-06-24 21:35:54 +0300

    create api endpoint for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,19 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
+    respond_to do |format|
+      format.html
+      format.json { render json: @users }
+    end
   end
 
   def show
     @user = User.find(params[:id])
+    @recent_posts = @user.recent_3_posts
+    respond_to do |format|
+      format.html
+      format.json { render json: @recent_posts }
+    end
   end
 
   def edit
